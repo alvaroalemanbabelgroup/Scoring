1a3fd2d4e1c4870f47e782d89723cd7bbfe01fe2
Alvaro Aleman Rando <alvaro.aleman@babelgroup.com>
2023-03-22 11:31:13 +0100

    Añadida persona con su id y todo al programa

--- src/main/java/com/example/scoring/models/Persona.java
+++ src/main/java/com/example/scoring/models/Persona.java
@@ -1,5 +1,6 @@
 package com.example.scoring.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,11 +22,13 @@ public class Persona {
     private boolean direccionDomicilioSameAsNotificacion = true;
     private Direccion direccion_notificacion;
     private String nif;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date fecha_nacimiento;
     private String nacionalidad;
-    @Min(0)
+    @Min(1)
     @Max(8)
     private int scoring;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date fecha_scoring;
     private List<Telefono> telefonoList;
 
--- src/main/java/com/example/scoring/models/RentaAnual.java
+++ src/main/java/com/example/scoring/models/RentaAnual.java
@@ -1,5 +1,6 @@
 package com.example.scoring.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,12 +11,15 @@ import java.util.Date;
 @Setter
 public class RentaAnual {
     private int renta_id;
-    private int persona_id;
+   // private int persona_id;
+
+    private Persona persona;
     private Profesion profesion;
     private int anio;
     private int importe;
     private int is_cuenta_propia;
     private String iae;
     private String cif_empleador;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date fecha_inicio_empleo;
 }
